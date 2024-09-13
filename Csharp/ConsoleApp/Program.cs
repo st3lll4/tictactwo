f024abd2a26ab9a0499e1b2c21c9ab5ae3049990
Stella Tukia <148345190+sttuki@users.noreply.github.com>
2024-09-13 10:52:32 +0300

    what the

--- Csharp/ConsoleApp/Program.cs
+++ Csharp/ConsoleApp/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using BLL;
+
+Console.WriteLine("Hello, Dinosaur!"); 
+
+var calc = new CalculatorBrain();
+
+calc.Number1 = 1;
+calc.Number2 = 2;
+
+var res = calc.Add();
+var res2 = calc.Subtract();
+
+Console.WriteLine($"{calc.Number1} + {calc.Number2} = {res}");
+Console.WriteLine($"{calc.Number1} - {calc.Number2} = {res2}");
+
