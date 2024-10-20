511a0e96f55ba7e40ea436c0d814d96523a558af
stella.t <sttuki@taltech.ee>
2024-10-20 16:22:54 +0300

    need help

--- tic-tac-two/Configs/ConfigRepositoryJson.cs
+++ tic-tac-two/Configs/ConfigRepositoryJson.cs
@@ -2,5 +2,13 @@ namespace Configs;
 
 public class ConfigRepositoryJson : IConfigRepository
 {
-    
+    public List<string> GetSavedConfigurations()
+    {
+        throw new NotImplementedException();
+    }
+
+    public GameConfiguration GetConfigurationByName(string name)
+    {
+        throw new NotImplementedException();
+    }
 }
