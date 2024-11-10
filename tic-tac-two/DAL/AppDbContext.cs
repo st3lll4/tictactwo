143b6c0f36a21048f69bc3303d8379deb6265e8a
stella.t <sttuki@taltech.ee>
2024-11-10 21:10:48 +0200

    migration done

--- tic-tac-two/DAL/AppDbContext.cs
+++ tic-tac-two/DAL/AppDbContext.cs
@@ -1,9 +1,13 @@
+using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace DAL;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<Game> Games { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Configuration> Configurations { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
--- tic-tac-two/Domain/User.cs
+++ tic-tac-two/Domain/User.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Domain;
 
-public class User
+public class User : BaseEntity
 {
     [MaxLength(128)] 
     public string UserName { get; set; } = default!;
